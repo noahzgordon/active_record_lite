8213c3e31a4af8001ebc36a92eed84f6ed7213ea
Noah Gordon <tortuga.ng@gmail.com>
2014-07-25 12:23:58 -0400

    Add .where method through Searchable module.

--- lib/active_record_lite/02_searchable.rb
+++ lib/active_record_lite/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map do |attr_name, value|
+      "#{attr_name} = ?"
+    end.join ' AND '
+
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    results.map { |hash| self.new(hash) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
